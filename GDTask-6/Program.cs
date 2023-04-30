01e761070fadaa0269e527c6b09b729ddb019263
MetaphysicsNecrosis <MetaphysicsNecrosis@DESKTOP-A9QI9P7>
2023-04-30 13:39:51 +0300

    Добавьте файлы проекта.

---
+++ GDTask-6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GDTask_6
+{
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String s = Console.ReadLine();
+            String s1 = Regex.Replace(s, "(?i)[aeiouyауоыиэяюёе]", "");
+            Console.WriteLine(s1);
+            Console.ReadKey();
+        }
+    }
+}
